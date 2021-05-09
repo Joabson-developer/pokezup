92a2e998d604cf6e403c0a6cd1ba193c95031dd2
Joabson-developer <83815302+Joabson-developer@users.noreply.github.com>
2021-05-09 03:44:48 -0300

    Updates

---
+++ precache-manifest.c103e61e3277c70e8b512d0bd6b28225.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1489fd70c246340d163db24dc8ecda42",
+    "url": "/pokezup.io/index.html"
+  },
+  {
+    "revision": "9db46cb5d30ccc010fcd",
+    "url": "/pokezup.io/static/js/2.9be935b6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pokezup.io/static/js/2.9be935b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a54e7aa04a5b673556d5",
+    "url": "/pokezup.io/static/js/main.0aa73136.chunk.js"
+  },
+  {
+    "revision": "b2d2ff07d93d07c32d2f",
+    "url": "/pokezup.io/static/js/runtime-main.64a82032.js"
+  }
+]);
